9a160b56dc986d8ab0f15b7508d28bef16e400b3
alejandrobrazabarba <alejandrobrazabarba@gmail.com>
2016-12-05 16:39:10 +0100

    image_processing_node.py converted to ros node
    ROS node created in image_processing_node.py starting from code in
    prueba_opencv_pi.py

--- src/image_processing_node.py
+++ src/image_processing_node.py
@@ -7,58 +7,66 @@ import numpy
 import cv2
 import time
 
-# Initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 4
-rawCapture = PiRGBArray(camera, size=(640, 480))
+# ROS specific imports
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-# Allow the camera to warmup
-time.sleep(0.1)
 
-img_width = 640
-img_height = 480
+class ImageProcessing:
 
-# Define the regions of the image to analyse
-NUM_REGIONS = 10
-reg_vert_offset = 400
-reg_vert_width = 20
-reg_vert_end = reg_vert_offset + reg_vert_width
-reg_horiz_width = img_width/NUM_REGIONS
-reg_horiz_divs = numpy.arange(0,img_width+1,img_width/NUM_REGIONS)
-reg_num_pixels = 128*20
-intensity_threshold = 30
+    def __init__(self):
+        # Initialize node
+        rospy.init_node('image_processing', anonymous=False)
+        self.bridge = CvBridge()
+        self.image_pub = rospy.Publisher("processed_image", Image)
+        # Initialize the camera and grab a reference to the raw camera capture
+        self.camera = PiCamera()
+        self.camera.resolution = (640, 480)
+        self.camera.framerate = 4
+        self.rawCapture = PiRGBArray(camera, size=(640, 480))
 
-GREEN = (0,255,0)
-RED = (0,0,255)
+        # Allow the camera to warmup
+        time.sleep(0.1)
 
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        self.img_width = 640
+        self.img_height = 480
 
-    image = frame.array
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Define the regions of the image to analyse
+        self.NUM_REGIONS = 10
+        self.reg_vert_offset = 400
+        self.reg_vert_width = 20
+        self.reg_vert_end = self.reg_vert_offset + self.reg_vert_width
+        self.reg_horiz_width = self.img_width/self.NUM_REGIONS
+        self.reg_horiz_divs = numpy.arange(0, self.img_width+1, self.img_width/self.NUM_REGIONS)
+        self.reg_num_pixels = 128*20
+        self.intensity_threshold = 30
 
-    # For each region of interest in the image
-    for i in range(0,NUM_REGIONS):
-        # Calculate average pixel intensity
-        avg_int = numpy.median( gray[reg_vert_offset:reg_vert_end, reg_horiz_divs[i]:reg_horiz_divs[i+1]] )
-        # Select color for rectangle depending on pixel intensity
-        reg_rect_color = RED if avg_int < intensity_threshold else GREEN
-        # Draw rectangle
-        cv2.rectangle(image, (reg_horiz_divs[i],reg_vert_offset),
-                     (reg_horiz_divs[i+1]-1, reg_vert_end-1),reg_rect_color,1)
+        self.GREEN = (0, 255, 0)
+        self.RED = (0, 0, 255)
 
-    # cv2.rectangle(frame, (330,0), (630,430),(0,255,0),3)
-    # Histogram equalization
-    # equR = cv2.equalizeHist(frame[:,:,2])
-    # equG = cv2.equalizeHist(frame[:,:,1])
-    # equB = cv2.equalizeHist(frame[:,:,0])
-    # res = numpy.hstack((frame,cv2.merge((equB,equG,equR))))
-    rawCapture.truncate(0)
+    def main(self):
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
-    # Display the resulting frame come here
-    cv2.imshow('Image', image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            image = frame.array
+            # Our operations on the frame come here
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-cv2.destroyAllWindows()
+            # For each region of interest in the image
+            for i in range(0, self.NUM_REGIONS):
+                # Calculate average pixel intensity
+                avg_int = numpy.median(gray[self.reg_vert_offset:self.reg_vert_end,
+                                       self.reg_horiz_divs[i]:self.reg_horiz_divs[i+1]])
+                # Select color for rectangle depending on pixel intensity
+                reg_rect_color = RED if avg_int < self.intensity_threshold else GREEN
+                # Draw rectangle
+                cv2.rectangle(image, (self.reg_horiz_divs[i], self.reg_vert_offset),
+                                     (self.reg_horiz_divs[i+1]-1, self.reg_vert_end-1), reg_rect_color, 1)
+
+                self.image_pub.publish(self.bridge.cv2_to_imgmsg(image, "bgr8"))
+                rawCapture.truncate(0)
+
+
+if __name__ == '__main__':
+    imageProcessing = ImageProcessing()
+    imageProcessing.main()
